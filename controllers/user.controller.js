d7486e29b256ab450a55e63127dcaed2e94090a0
C0D1NGD0J0 <web-dev@United4Life.local>
2017-04-07 16:05:34 +0100

    simple login process created with validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,20 @@
 const User = require('../models/User.model');
 
 exports.register = (req, res) => {
-	
+	let newUser = new User();
+
+	newUser.username = req.body.username; 
+	newUser.email		 = req.body.email;
+	newUser.password = req.body.password;
+
+	newUser.save((err, user) => {
+		if(err){
+			console.log(err.message);
+			console.log(err.errors.username.message);
+			res.send('Error registering user');
+		} else {
+			console.log(user);
+			res.redirect('/signup.html');
+		}
+	});
 }
--- models/User.model.js
+++ models/User.model.js
@@ -1,9 +1,19 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-var UserSchema = new Schema({
-  username: String,
-  email: String,
+const minlength = [5, "The value provided is doesn't meet the min-length required to signup"];
+
+let UserSchema = new Schema({
+  username: {
+  	type: String,
+  	require: true,
+  	minlength: minlength
+  },
+  email: {
+  	type: String,
+  	required: true,
+  	unique: true
+  },
   password: String
 });
 
